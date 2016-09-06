f8de41c5265824bb8ed8c06396747ad7e4d1607b
Taiki Arakawa <at.090313@gmail.com>
2016-09-06 12:23:05 +0900

    リセット/ 戻る / クリア処理

--- src/app.js
+++ src/app.js
@@ -1,5 +1,5 @@
 var size;
-var level = [
+var level_1 = [
   [1, 1, 1, 1, 1, 1, 1],
   [1, 1, 0, 0, 0, 0, 1],
   [1, 1, 3, 0, 2, 0, 1],
@@ -8,6 +8,21 @@ var level = [
   [1, 0, 0, 1, 1, 1, 1],
   [1, 1, 1, 1, 1, 1, 1]
 ];
+
+var init_map = [            // 初期化用のマップ配列
+  [1, 1, 1, 1, 1, 1, 1],
+  [1, 1, 0, 0, 0, 0, 1],
+  [1, 1, 3, 0, 2, 0, 1],
+  [1, 0, 0, 4, 0, 0, 1],
+  [1, 0, 3, 1, 2, 0, 1],
+  [1, 0, 0, 1, 1, 1, 1],
+  [1, 1, 1, 1, 1, 1, 1]
+];
+
+var init_crates = [];       // 初期化用の木箱スプライト配列
+var back_map = [];          // 戻り処理用のマップ配列
+var back_crates = [];       // 戻り処理用の木箱スプライト配列
+
 var playerPosition; //マップ内のプレイやの位置(ｘ、ｙ)を保持する
 var playerSprite; //プレイヤーのスプライト
 var cratesArray = []; //配置した木箱のスプライトを配列に保持する
@@ -19,11 +34,9 @@ var swipeTolerance = 10;//スワイプかを判断する閾値
 var gameScene = cc.Scene.extend({
   onEnter: function() {
     this._super();
-
     var layer0 = new gameLayer();
     layer0.init();
     this.addChild(layer0);
-
   }
 });
 
@@ -50,8 +63,9 @@ var gameLayer = cc.Layer.extend({
 
     for (i = 0; i < 7; i++) {　　　　　　
       cratesArray[i] = [];　 //配列オブジェクトの生成
+      init_crates[i] = [];
       for (j = 0; j < 7; j++) {
-        switch (level[i][j]) {
+        switch (level_1[i][j]) {
           case 4:
           case 6:
             playerSprite = cc.Sprite.create(cache.getSpriteFrame("player.png"));
@@ -63,6 +77,8 @@ var gameLayer = cc.Layer.extend({
               y: i
             };　　　　　　　　　　　　
             cratesArray[i][j] = null;　 //playerがいるので、その場所には木箱はないのでnullを代入する
+            var copy = cratesArray[i][j];
+            init_crates[i][j] = copy;
             break;
           case 3:
           case 5:
@@ -71,60 +87,61 @@ var gameLayer = cc.Layer.extend({
             crateSprite.setScale(5);
             this.addChild(crateSprite);
             cratesArray[i][j] = crateSprite;//(i,j)の位置にcrateSpriteを入れる
+            var copy = cratesArray[i][j];
+            init_crates[i][j] = copy;
             break;
           default:
             cratesArray[i][j] = null;//木箱のコード以外の場合は、その場所に木箱がない値としてnullを代入する
+            var copy = cratesArray[i][j];
+            init_crates[i][j] = copy;
             break;
         }
       }
     }
-    //return true;
     cc.eventManager.addListener(listener, this);
+    cc.eventManager.addListener({
+      event: cc.EventListener.KEYBOARD,
+      onKeyPressed: function(keyCode, event){
+        if(keyCode == 82) reset();          // R-Keyでリセット
+        if(keyCode == 66) back();           // B-Keyでバック
+      }
+    }, this);
   },
 });
 
 var listener = cc.EventListener.create({
-event: cc.EventListener.TOUCH_ONE_BY_ONE,
-swallowTouches: true,
-onTouchBegan:function (touch,event) {
-startTouch = touch.getLocation();
-return true;
-},
-onTouchEnded:function(touch, event){
-endTouch = touch.getLocation();
-swipeDirection();
-}
+  event: cc.EventListener.TOUCH_ONE_BY_ONE,
+  swallowTouches: true,
+  onTouchBegan:function (touch,event) {
+    startTouch = touch.getLocation();
+    return true;
+  },
+  onTouchEnded:function(touch, event){
+    endTouch = touch.getLocation();
+    swipeDirection();
+  }
 });
+
 //スワイプ方向を検出する処理
 function swipeDirection(){
-
-
     var distX = endTouch.x - startTouch.x ;
     var distY = endTouch.y - startTouch.y ;
     if(Math.abs(distX)+Math.abs(distY)>swipeTolerance){
         if(Math.abs(distX)>Math.abs(distY)){
             if(distX>0){//右方向移動
-              //playerSprite.setPosition(playerSprite.getPosition().x+25,playerSprite.getPosition().y);
                 move(1,0);
             }
             else{//左方向移動
-              //playerSprite.setPosition(playerSprite.getPosition().x-25,playerSprite.getPosition().y);
                 move(-1,0);
             }
         }
         else{
-        //  console.log("endTouch.y "+endTouch.y );
-        //  console.log("startTouch.y "+startTouch.y );
-        //  console.log("distY "+ distY );
             if(distY>0){ //上方向移動
-            //  playerSprite.setPosition(playerSprite.getPosition().x,playerSprite.getPosition().y+25);
-               console.log("上 move(0,-1) distY "+ distY );
+              //console.log("上 move(0,-1) distY "+ distY );
               move(0,-1);
-
             }
             else{ //下方向移動
-              //playerSprite.setPosition(playerSprite.getPosition().x,playerSprite.getPosition().y-25);
-              console.log("下 move(0,1) distY "+ distY );
+              //console.log("下 move(0,1) distY "+ distY );
               move(0,1);
             }
         }
@@ -132,31 +149,124 @@ function swipeDirection(){
 }
 
 function move(deltaX,deltaY){
-switch(level[playerPosition.y+deltaY][playerPosition.x+deltaX]){
-    case 0:
-    case 2:
-        level[playerPosition.y][playerPosition.x]-=4;
-        playerPosition.x+=deltaX;
-        playerPosition.y+=deltaY;
-        level[playerPosition.y][playerPosition.x]+=4;
-        playerSprite.setPosition(165+25*playerPosition.x,185-25*playerPosition.y);
-    break;
-    case 3:
-    case 5:
-        if(level[playerPosition.y+deltaY*2][playerPosition.x+deltaX*2]==0 ||
-           level[playerPosition.y+deltaY*2][playerPosition.x+deltaX*2]==2){
-            level[playerPosition.y][playerPosition.x]-=4;
-            playerPosition.x+=deltaX;
-            playerPosition.y+=deltaY;
-            level[playerPosition.y][playerPosition.x]+=1;
-            playerSprite.setPosition(165+25*playerPosition.x,185-25*playerPosition.y);
-            level[playerPosition.y+deltaY][playerPosition.x+deltaX]+=3;
-            var movingCrate = cratesArray[playerPosition.y][playerPosition.x];
-            movingCrate.setPosition(movingCrate.getPosition().x+25*deltaX,movingCrate.
-            getPosition().y-25*deltaY);
-            cratesArray[playerPosition.y+deltaY][playerPosition.x+deltaX]=movingCrate;
-            cratesArray[playerPosition.y][playerPosition.x]=null;
-        }
-        break;
+  back_up();      // 動きのバックアップ
+  switch(level_1[playerPosition.y+deltaY][playerPosition.x+deltaX]){
+      case 0:
+      case 2:
+          level_1[playerPosition.y][playerPosition.x]-=4;
+          playerPosition.x+=deltaX;
+          playerPosition.y+=deltaY;
+          level_1[playerPosition.y][playerPosition.x]+=4;
+          playerSprite.setPosition(165+25*playerPosition.x,185-25*playerPosition.y);
+          break;
+      case 3:
+      case 5:
+          if(level_1[playerPosition.y+deltaY*2][playerPosition.x+deltaX*2]==0 ||
+          level_1[playerPosition.y+deltaY*2][playerPosition.x+deltaX*2]==2){
+          level_1[playerPosition.y][playerPosition.x]-=4;
+          playerPosition.x+=deltaX;
+          playerPosition.y+=deltaY;
+          level_1[playerPosition.y][playerPosition.x]+=1;
+          playerSprite.setPosition(165+25*playerPosition.x,185-25*playerPosition.y);
+          level_1[playerPosition.y+deltaY][playerPosition.x+deltaX]+=3;
+          var movingCrate = cratesArray[playerPosition.y][playerPosition.x];
+          movingCrate.setPosition(movingCrate.getPosition().x+25*deltaX,movingCrate.
+          getPosition().y-25*deltaY);
+          cratesArray[playerPosition.y+deltaY][playerPosition.x+deltaX]=movingCrate;
+          cratesArray[playerPosition.y][playerPosition.x]=null;
+          }
+          break;
+  }
+  complete_check()      //クリアの確認
+}
+
+// リセット処理
+function reset(){
+  for (var i = 0; i < 7; i++){
+    for (var j = 0; j < 7; j++){
+      var copy = init_map[i][j];
+      level_1[i][j] = copy;
+      switch (level_1[i][j]) {
+        case 4:
+        case 6:
+          playerSprite.setPosition(165 + 25 * j, 185 - 25 * i);
+          playerPosition = {
+            x: j,
+            y: i
+          };
+          var copy = init_crates[i][j];
+          cratesArray[i][j] = copy;
+          break;
+        case 3:
+        case 5:
+          var copy = init_crates[i][j];
+          cratesArray[i][j] = copy;
+          var crateSprite = cratesArray[i][j];
+          crateSprite.setPosition(165 + 25 * j, 185 - 25 * i);
+          break;
+        default:
+          var copy = init_crates[i][j];
+          cratesArray[i][j] = copy;
+          break;
+      }
     }
+  }
+}
+
+// １行動前のバックアップ
+function back_up(){
+  for (var i = 0; i < 7; i++){
+    back_map[i] = [];
+    back_crates[i] = [];
+    for (var j = 0; j < 7; j++){
+      var copy1 = level_1[i][j];
+      back_map[i][j] = copy1;
+      var copy2 = cratesArray[i][j];
+      back_crates[i][j] = copy2;
+    }
+  }
+}
+
+// 1行動前に戻る処理
+function back(){
+  for (var i = 0; i < 7; i++){
+    for (var j = 0; j < 7; j++){
+      var copy1 = back_map[i][j];
+      level_1[i][j] = copy1;
+      switch (level_1[i][j]) {
+        case 4:
+        case 6:
+          playerSprite.setPosition(165 + 25 * j, 185 - 25 * i);
+          playerPosition = {
+            x: j,
+            y: i
+          };
+          var copy2 = back_crates[i][j];
+          cratesArray[i][j] = copy2;
+          break;
+        case 3:
+        case 5:
+          var copy2 = back_crates[i][j];
+          cratesArray[i][j] = copy2;
+          var crateSprite = cratesArray[i][j];
+          crateSprite.setPosition(165 + 25 * j, 185 - 25 * i);
+          break;
+        default:
+          var copy2 = back_crates[i][j];
+          cratesArray[i][j] = copy2;
+          break;
+      }
+    }
+  }
+}
+
+// クリアチェック処理
+function complete_check(){
+  var game_f = 0;
+  for (var i = 0; i < 7; i++) {
+    for (var j = 0; j < 7; j++) {
+      if (level_1[i][j] == 2) game_f +=1;
+    }
+  }
+  if (game_f == 0) reset();
 }
